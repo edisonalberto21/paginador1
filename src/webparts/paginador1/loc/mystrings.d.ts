db7e908a4f0cb593367a451a3feff2c130ed5d45
Edison Giraldo <egiraldor@intergrupo.com>
2021-10-27 02:22:50 -0500

    first commit

---
+++ src/webparts/paginador1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPaginador1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Paginador1WebPartStrings' {
+  const strings: IPaginador1WebPartStrings;
+  export = strings;
+}
